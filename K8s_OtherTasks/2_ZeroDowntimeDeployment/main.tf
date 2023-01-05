2071cb74a3e20d6c5f8cf5c9133de653070530e7
dmitriyshub <d.shtranvasser@gmail.com>
2023-01-05 13:06:53 +0200

    second public ECR

--- K8s_OtherTasks/2_ZeroDowntimeDeployment/main.tf
+++ K8s_OtherTasks/2_ZeroDowntimeDeployment/main.tf
@@ -30,7 +30,26 @@ resource "aws_ecrpublic_repository" "public-ecr" {
     name = "dmitriyshub-zero-downtime-app"
   }
 }
-# ecr resource metadata output
+
+# public ecr
+resource "aws_ecrpublic_repository" "public-ecr2" {
+  provider = aws.us_east_1
+
+  repository_name = "dmitriyshub-zero-downtime-app-2"
+
+  catalog_data {
+    about_text        = "simple flask webserver 2"
+    description       = "docker image for k8s 2"
+  }
+
+  tags = {
+    owner = "terraform"
+    env = "test"
+    name = "dmitriyshub-zero-downtime-app-2"
+  }
+}
+
+# ecr 1 resource metadata output
 output "public-ecr-arn" {
   value = aws_ecrpublic_repository.public-ecr.arn
 }
@@ -39,4 +58,17 @@ output "public-ecr-id" {
 }
 output "public-ecr-uri" {
   value = aws_ecrpublic_repository.public-ecr.repository_uri
+}
+
+
+
+# ecr 2 resource metadata output
+output "public-ecr-arn-2" {
+  value = aws_ecrpublic_repository.public-ecr2.arn
+}
+output "public-ecr-id-2" {
+  value = aws_ecrpublic_repository.public-ecr2.id
+}
+output "public-ecr-uri-2" {
+  value = aws_ecrpublic_repository.public-ecr2.repository_uri
 }
